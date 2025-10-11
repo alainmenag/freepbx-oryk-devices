e7c78dc315ae3aa637cd1e062a66b35347238200
Alain <alainmenag@gmail.com>
2025-10-11 15:35:00 -0400

    Initial commit

---
+++ functions.inc.php
@@ -0,0 +1,14 @@
+<?php
+// functions.inc.php
+
+if (!function_exists('Oryk_devices_configpageinit'))
+{
+	function Oryk_devices_configpageinit($page)
+	{
+		// $freepbx = \FreePBX::create();
+		// $oryk = $freepbx->oryk_devices;
+
+		// $user = $oryk->getUser();
+	}
+}
+?>
---
+++ views/devices.php
@@ -0,0 +1,11 @@
+<div class="container-fluid">
+	<div class="fpbx-container">
+		<div class="display full-border">
+			<form class="fpbx-submit" name="submitSettings" action="config.php?display=oryk_devices"  method="post">
+
+				<h2>Coming soon.</h2>
+
+			</form>
+		</div>
+	</div>
+</div>
